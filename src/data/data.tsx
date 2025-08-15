03b2ec6206d61cbc2de67113176756dd91ad8330
andreiprigoana <122204698+andreiprigoana@users.noreply.github.com>
2025-08-15 19:57:29 +0000

    Refactoring

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -53,8 +53,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'Andrei Prigoana',
+  description: "",
 };
 
 /**
